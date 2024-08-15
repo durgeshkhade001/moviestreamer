e2aaa9c6c9bdccfae90933216804efac5cbbc7a2
Durgesh Khade <durgeshkhade001@gmail.com>
2024-08-15 17:18:53 +0530

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 5001;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
